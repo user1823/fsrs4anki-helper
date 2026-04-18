dfdd6c987e6974aca997aded7fa1cc3e474c3645
user1823 <92206575+user1823@users.noreply.github.com>
2026-04-18 14:29:18 +0530

    Use the actual decay if higher than my configured one

--- schedule/reschedule.py
+++ schedule/reschedule.py
@@ -462,7 +462,9 @@ def reschedule_card(cid, fsrs: FSRS, recompute=False, auto_reschedule=False):
     if card.type == CARD_TYPE_REV:
         fsrs.set_card(card)
         fsrs.set_fuzz_factor(cid, card.reps)
-        decay = 0.3984  # hard-coded because I believe that FSRS hasn't learnt the actual delay for some of my decks yet
+        decay = max(
+            get_decay(card), 0.3984
+        )  # used a min value because I believe that FSRS hasn't learnt the actual delay for some of my decks yet
         adr = adr_dr(s, d)
         card.desired_retention = fsrs.desired_retention
         due_before = card.odue if card.odid else card.due
